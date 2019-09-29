eb4b0a1ef56c5fb0d373fba329440c395e3bbd79
Chervin Tanilon <54093089+chervintani@users.noreply.github.com>
2019-09-29 12:12:42 +0800

    Update Accounts.java

--- Accounts.java
+++ Accounts.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package trycatchExperiment;
+package enrollcherv;
 
 import java.util.Scanner;
 
@@ -26,13 +26,13 @@ public class Accounts {
         id = this.id;
     }
 
-    public Accounts(int id, String username, String pass, String conPass) {
-        id = this.id;
+    public Accounts(int id, String username, String pass) {
+        this.id = id;
         this.username = username;
         this.pass = pass;
-        this.conPass = conPass;
     }
 
+    
     public String getUsername() {
         return username;
     }
