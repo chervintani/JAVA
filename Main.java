4b5177df8336041db20a5934c952aeb96de0ccbb
tanilonch_sd2023@CEACCS.NET <tanilonch_sd2023@CEACCS.NET>
2019-09-30 07:34:21 -0400

    Stopped at line 442 in StudentEnroll

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package enrollcherv;
+package trycatchexperiment;
 
 import java.io.IOException;
 
--- StudentEnroll.java
+++ StudentEnroll.java
@@ -3,10 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package enrollcherv;
+package trycatchexperiment;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -46,15 +48,30 @@ public class StudentEnroll {
 
     int allSchedSuc = 0;
     int courseSuc = 0;
+    int courseSuc2 = 0;
     String course = "";
     int unitSuc = 0;
+    int unitSuc2 = 0;
     String units = "";
     int schedSuc = 0;
     String sched = "";
     String choice = "";
 //    String newUser = "";
-
+    String newName = "";
+    String oldName = "";
+    String newName2 = "";
+    String oldName2 = "";
+    String oldNum = "";
+    String newNum2 = "";
+    String oldSub = "";
+    String newSub = "";
+    int oldUnit = 0;
+    String newUnit = "";
+    int schedSuc2 = 0;
+    String oldSched = "";
+    String newSched = "";
     Accounts account = new Accounts(id);
+    String delInfo = "";
     Infos info = new Infos();
     Schedules schedObj = new Schedules();
     ArrayList<String> accountInfo = new ArrayList<String>();
@@ -66,7 +83,7 @@ public class StudentEnroll {
     public void options() throws IOException {
         while (true) {
             String options = "";
-            System.out.print("1. Create\n2. Retrieve\n3. Update\n4. Delete\nSelect options: ");
+            System.out.print("1. Create\n2. Retrieve\n3. Update\n4. Delete\n5. Search\n6. Save\nSelect options: ");
             options = scan.nextLine();
             System.out.println(options);
             switch (options) {
@@ -74,12 +91,19 @@ public class StudentEnroll {
                     account.userVal();
                     account.passVal();
                     account.reset();
-                    info.fNameVal();
-                    info.lNameVal();
-                    info.ageVal();
-                    info.reset();
-                    scheds();
-                    saving();
+                    System.out.println("Do you want to add personal information? y/n");
+                    String cho = scan.nextLine();
+                    if (cho.equals("y") || cho.equals("yes") || cho.equals("YES")) {
+                        info.lNameVal();
+                        info.ageVal();
+                        info.reset();
+                    }
+                    System.out.println("Do you want to add schedule? y/n");
+                    String cho2 = scan.nextLine();
+                    if (cho2.equals("y") || cho2.equals("yes") || cho2.equals("YES")) {
+                        scheds();
+                    }
+
                     reset();
                     break;
                 case "2":
@@ -185,7 +209,7 @@ public class StudentEnroll {
                     String option3 = scan.nextLine();
                     if (option3.equals("1")) {
                         //FOR THE ACCOUNTS YOU WANT TO UPDATE
-                        displayAcc();
+                        displayInfo();
                         System.out.print("Enter ID you want to update: ");
                         String idInp = scan.nextLine();
                         //I STOPPED RIGHT HERE I NEED REST!!!!!!!!!!!!!
@@ -200,12 +224,14 @@ public class StudentEnroll {
                                         for (int j = 0; j < newfName.length(); j++) {
                                             char c = newfName.charAt(j);
                                             if (Character.isDigit(c) || newfName.equals(" ")) {
-                                                throw new Exception("Invalid first name input!");
+                                                throw new Exception("Invalid new first name input!");
                                             } else {
-                                                modifyFile("infos.txt", infoList.get(i).getfName(), newfName);
+                                                oldName = infoList.get(i).getfName();
+                                                newName = newfName;
+//                                                modifyFile("infos.txt", infoList.get(i).getfName(), newfName);
                                                 infoList.get(i).setfName(newfName);
-                                                System.out.println("Updated successfully!");
-                                                displayInfo();
+//                                                System.out.println("Updated successfully!");
+//                                                displayInfo();
                                                 fnSuc = 1;
                                             }
                                             break;
@@ -214,28 +240,220 @@ public class StudentEnroll {
                                         System.out.println(e.getMessage());
                                     }
                                 }
+                                while (lnSuc == 0) {
+                                    try {
+                                        System.out.print("Enter new lastname: ");
+                                        String newlName = scan.nextLine();
+                                        for (int l = 0; l < newlName.length(); i++) {
+                                            char ce = newlName.charAt(l);
+                                            if (Character.isDigit(ce) || newlName.equals(" ")) {
+                                                throw new Exception("Invalid new last name input!");
+                                            } else {
+                                                oldName2 = infoList.get(l).getlName();
+                                                newName2 = newlName;
+//                                                modifyFile("infos.txt", infoList.get(i).getfName(), newfName);
+                                                infoList.get(l).setlName(newlName);
+                                                lnSuc = 1;
+                                            }
+                                            break;
+                                        }
+                                    } catch (Exception e) {
+                                        System.out.println(e.getMessage());
+                                    }
+                                }
+                                while (ageSuc == 0) {
+                                    try {
+                                        System.out.print("Enter new age: ");
+                                        String newAge = scan.nextLine();
+                                        if (newAge.matches("-?\\d+(\\.\\d+)?") == false) {
+                                            throw new Exception("Invalid new age input!");
+                                        } else {
+                                            try {
+                                                if (Integer.parseInt(newAge) < 1) {
+                                                    throw new Exception("Age should not be 0!");
+                                                } else {
+                                                    oldNum = infoList.get(i).getNum();
+                                                    newNum2 = newAge;
+//                                                modifyFile("infos.txt", infoList.get(i).getfName(), newfName);
+                                                    infoList.get(i).setNum(newAge);
+                                                    ageSuc = 1;
+//                                                    System.out.println("New information saved\n");
+                                                }
+                                            } catch (Exception e) {
+                                                System.out.println(e.getMessage());
+                                            }
+                                        }
+                                    } catch (Exception e) {
+                                        System.out.println(e.getMessage());
+                                    }
+                                }
                             }
+                        }
+                    } else if (option3.equals("2")) {
+                        displaySched();
+                        System.out.print("Enter ID you want to update: ");
+                        String idInp2 = scan.nextLine();
+                        for (int i = 0; i < schedList.size(); i++) {
+                            if (schedList.get(i).getId() == Integer.parseInt(idInp2)) {
+                                while (courseSuc2 == 0) {
+                                    try {
+                                        System.out.print("Enter new subject: ");
+                                        String newSub2 = scan.nextLine();
+//                                        for (int s = 0; s < newSub.length(); s++) {
+//                                            char co = newSub.charAt(s);
+//                                            if (Character.isDigit(co)) {
+//                                                throw new Exception("Invalid course input!");
+//                                            } else {
+                                        oldSub = schedList.get(i).getCourse();
+                                        newSub = newSub2;
+//                                                modifyFile("infos.txt", infoList.get(i).getfName(), newfName);
+                                        schedList.get(i).setCourse(newSub);
+                                        courseSuc2 = 1;
+//                                            }
+                                        break;
+//                                        }
+                                    } catch (Exception e) {
+                                        System.out.println(e.getMessage());
+                                    }
+                                }
+
+                                while (unitSuc2 == 0) {
+                                    try {
+                                        System.out.print("Enter new units: ");
+                                        String newUnit2 = fn.nextLine();
+//                                        if (newUnit.matches("-?\\d+(\\.\\d+)?") == false) {
+//                                            throw new Exception("Invalid units input, must be a number");
+//                                        } else {
+                                        oldUnit = schedList.get(i).getUnits();
+                                        newUnit = newUnit2;
+//                                                modifyFile("infos.txt", infoList.get(i).getfName(), newfName);
+                                        schedList.get(i).setUnits(Integer.parseInt(newUnit));
+                                        unitSuc2 = 1;
+//                                        }
+                                    } catch (Exception e) {
+                                        System.out.println(e.getMessage());
+                                    }
+                                }
+
+                                while (schedSuc2 == 0) {
+                                    try {
+                                        System.out.print("Enter new schedule: ");
+                                        String newSched2 = fn.nextLine();
+                                        if (newSched2.isEmpty()) {
+                                            throw new Exception("Please fill in your new schedule!");
+                                        } else {
+                                            oldSched = schedList.get(i).getSched();
+                                            newSched = newSched2;
+//                                                modifyFile("infos.txt", infoList.get(i).getfName(), newfName);
+                                            schedList.get(i).setSched(newSched);
+                                            schedSuc2 = 1;
+                                        }
+
+                                    } catch (Exception e) {
+                                        System.out.println(e.getMessage());
+                                    }
+                                }
+                            }
+                        }
+                        reset();
+                    }
+                    break;
+
+                case "4":
+                    System.out.println("Delete by:\n1. Personal Information\n3. Schedule\n:");
+                    String choice4 = scan.nextLine();
+                    if (choice4.equals("1")) {
+                        displayInfo();
+                        System.out.println("Delete personal information by ID: ");
+                        String delInfo = scan.nextLine();
+                        for (int i = 0; i < infoList.size(); i++) {
+                            if (infoList.get(i).getId() == Integer.parseInt(delInfo)) {
+                                infoList.remove(i);
+                            }
+                        }
+                    } else if (choice4.equals("2")) {
+                        displaySched();
+                        System.out.println("Delete schedule by ID: ");
+                        String delSched = scan.nextLine();
+                        for (int i = 0; i < schedList.size(); i++) {
+                            if (schedList.get(i).getId() == Integer.parseInt(delSched)) {
+                                schedList.remove(i);
+                                
+                                delInfo = schedList.get(i).toString();
+                                break;
+                            }
+                            
                         }
                     }
                     break;
-                case "4":
+
+                case "5":
+
                     break;
-                default:
+                case "6":
+                    modifyFile("infos.txt", oldName, newName);
+                    modifyFile("infos.txt", oldName2, newName2);
+                    modifyFile("infos.txt", oldNum, newNum2);
+                    modifyFile("schedule.txt", oldSub, newSub);
+                    modifyFile("schedule.txt", Integer.toString(oldUnit), newUnit);
+                    modifyFile("schedule.txt", oldSched, newSched);
+                    saving();
+                    del("schedule.txt");
+                    System.out.println("Updated Successfully");
                     break;
+
             }
         }
     }
 
     public void displayAcc() {
+        System.out.printf("%-20s%-20s%-20s\n", "ID", "USERNAME", "PASSWORD");
+        System.out.println(" ");
         for (Accounts a : acc) {
             System.out.println(a);
         }
     }
 
     public void displayInfo() {
+        System.out.printf("==================================================="
+                + "=================\n%-10s%-18s%-20s%-20s%-20s\n", "ID", "ACCOUNT ID",
+                "FIRSTNAME", "LASTNAME", "AGE");
+        System.out.println(" ");
         for (Infos i : infoList) {
             System.out.println(i);
         }
+    }
+
+    public void displaySched() {
+        System.out.printf("%-10s%-18s%-20s%-20s%-20s", "ID", "ACCOUNT ID", "COURSE", "UNITS", "SCHEDULE");
+        System.out.println(" ");
+        for (Schedules s : schedList) {
+            System.out.println(s);
+        }
+    }
+
+    public void del(String choices) throws FileNotFoundException, IOException {
+        String delChoice = choices;
+        File inputFile = new File(delChoice);
+        File tempFile = new File("myTempFile.txt");
+
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+        //STOPPED HERE FOR UPDATING THE FILE UPON DELETING SOMETHING FROM ARRAYLIST
+        String lineToRemove = delInfo;
+        String currentLine;
+
+        while ((currentLine = reader.readLine()) != null) {
+            // trim newline when comparing with lineToRemove
+            String trimmedLine = currentLine.trim();
+            if (trimmedLine.equals(lineToRemove)) {
+                continue;
+            }
+            writer.write(currentLine + System.getProperty("line.separator"));
+        }
+        writer.close();
+        reader.close();
+        boolean successful = tempFile.renameTo(inputFile);
     }
 
     static void modifyFile(String filePath, String oldString, String newString) {
@@ -411,6 +629,9 @@ public class StudentEnroll {
         allSchedSuc = 0;
         unitSuc = 0;
         schedSuc = 0;
+        schedSuc2 = 0;
         courseSuc = 0;
+        courseSuc2 = 0;
+        unitSuc2 = 0;
     }
 }
